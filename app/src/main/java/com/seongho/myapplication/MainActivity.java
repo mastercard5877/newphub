36c163f035ca118c21e3ecb2911e9384d906f5c3
Kimseongho <kshk1743@naver.com>
2019-03-23 00:24:32 +0900

    Update 3/23

--- app/src/main/java/com/seongho/myapplication/MainActivity.java
+++ app/src/main/java/com/seongho/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.seongho.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+    public void PhubClicked(View v){
+        Intent Phub = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.pornhub.com"));
+        startActivity(Phub);
+    }
+    public void XvidClicked(View v){
+        Intent Xvid = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.xvideo.es"));
+        startActivity(Xvid);
     }
 }
---
+++ app/src/main/java/com/seongho/myapplication/OpenAlertDialog.java
@@ -0,0 +1,34 @@
+package com.seongho.myapplication;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+
+public class OpenAlertDialog extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Information")
+                .setMessage("This is a Dialog")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+
+    public void show(FragmentManager supportFragmentManager) {
+    }
+}
